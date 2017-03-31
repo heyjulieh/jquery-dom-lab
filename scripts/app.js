b873cfbc5713663b21587ee6fb0b83ab42a1e6c2
heyjulieh <heyjulieh@gmail.com>
2017-03-31 03:12:42 -0700

    answered questions in jquery dom lab

--- scripts/app.js
+++ scripts/app.js
@@ -4,13 +4,24 @@ $( document ).ready(function whenLoaded(){
 
   // Your jQuery here
   // 1. Get the value "Hello There".
+  $('#greeting').text();
 
   // 2. Change (or set) the value to "Hola".
+  $('#greeting').text("Hola");
 
   // 3. Add an h1 to the page that says "A Spanish Greeting".
+  $('body').prepend("<h1>A Spanish Greeting</h1>");
 
   // 4. Add a second greeting ("Que tal?"), in a second column to the right of the first.
+  $('body').append("<div>Que tal?</div");
+
+  // what about prepending this again?
+  $('body').prepend("<div>Que tal?</div>");
+  // what will add do?
+  $('body').add('blue');
 
   // 5. Add the class "blue" to both greetings.
+  $('div').addClass("blue");
+  $('h1').addClass("blue");
 
 });
